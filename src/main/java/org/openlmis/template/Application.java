ea2b0fac5724f6d5f90f8a293f698bc85de77c4d
Chongsun Ahn <cahn922@yahoo.com>
2016-05-13 16:30:21 -0700

    Add basic i18n support
    Add i18n support to template. This simply defaults to English and shows how to use message source to get messages from the Java property file.

--- src/main/java/org/openlmis/template/Application.java
+++ src/main/java/org/openlmis/template/Application.java
@@ -2,11 +2,31 @@ package org.openlmis.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 }
--- src/main/java/org/openlmis/template/web/ServiceNameController.java
+++ src/main/java/org/openlmis/template/web/ServiceNameController.java
@@ -1,14 +1,28 @@
 package org.openlmis.template.web;
 
 import org.openlmis.template.util.ServiceSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ServiceNameController {
 
+    @Autowired
+    private MessageSource messageSource;
+    
     @RequestMapping("/")
     public ServiceSignature index() {
         return new ServiceSignature(ServiceSignature.SERVICE_NAME, ServiceSignature.SERVICE_VERSION);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return messageSource.getMessage("msg.hello.world", null, LocaleContextHolder.getLocale());
     }
 }
