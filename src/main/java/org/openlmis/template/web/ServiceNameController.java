64da190a7ba48f32759d2653cd071387fe7a0b00
Chongsun Ahn <cahn922@yahoo.com>
2016-05-25 11:52:45 -0700

    OLMIS-676 Add logging to syslog in Spring Boot app
    Add logging to container's syslog in Spring Boot's logbook configuration. Add some test log statements using SLF4J.

--- src/main/java/org/openlmis/template/web/ServiceNameController.java
+++ src/main/java/org/openlmis/template/web/ServiceNameController.java
@@ -1,28 +1,31 @@
 package org.openlmis.template.web;
 
 import org.openlmis.template.util.ServiceSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 public class ServiceNameController {
+
+    Logger logger = LoggerFactory.getLogger(ServiceNameController.class);
 
     @Autowired
     private MessageSource messageSource;
     
     @RequestMapping("/")
     public ServiceSignature index() {
+        logger.info("Returning service name and version");
         return new ServiceSignature(ServiceSignature.SERVICE_NAME, ServiceSignature.SERVICE_VERSION);
     }
 
     @RequestMapping("/hello")
     public String hello() {
+        logger.info("Returning hello world message");
         return messageSource.getMessage("msg.hello.world", null, LocaleContextHolder.getLocale());
     }
 }
