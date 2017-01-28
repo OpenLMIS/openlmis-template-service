e8cb9c259029793295b7b897a634a2f14862abb3
Ben Leibert <ben.leibert@villagereach.org>
2017-01-28 10:13:48 -0800

    OLMIS-1605: Add @TypeName to Widget
    JaVers' @TypeName annotation is optional, but recommended for use.

--- src/main/java/org/openlmis/template/domain/Widget.java
+++ src/main/java/org/openlmis/template/domain/Widget.java
@@ -3,12 +3,14 @@ package org.openlmis.template.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.javers.core.metamodel.annotation.TypeName;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@TypeName("Widget")
 @Table(name = "widget", schema = "template")
 @NoArgsConstructor
 public class Widget extends BaseEntity {
