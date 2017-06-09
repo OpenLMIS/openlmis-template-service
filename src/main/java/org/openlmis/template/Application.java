80a7c4c715d1ad4f8587b303a4fab2037df5756e
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-09 13:21:18 +0200

    OLMIS-2611: fixed import declaration

--- src/main/java/org/openlmis/template/Application.java
+++ src/main/java/org/openlmis/template/Application.java
@@ -47,7 +47,7 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import java.util.Locale;
 
-import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.LocaleUtils.toLocale;
 
 @SpringBootApplication
