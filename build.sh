61506331665eb40038f7b10699637195381b3337
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-28 16:07:22 +0200

    OLMIS-2752: updated postgres + openlmis/dev versions

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Sync with Transifex
-./sync_transifex.sh
+/transifex/sync_transifex.sh \
+  --resource openlmis-requisition.messages \
+  --pattern 'src/main/resources/messages_<lang>.properties' \
+  --source-file src/main/resources/messages_en.properties
+
 
 # Run Gradle build
 gradle clean build
