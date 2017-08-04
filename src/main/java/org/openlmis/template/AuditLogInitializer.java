da240ebcdd1f545bff396075b5089161ddcb86c9
Łukasz Lewczyński <lukaslew@users.noreply.github.com>
2017-08-04 13:42:35 +0200

    OLMIS-2534: Fixed potential huge performance
    When the system starts, the javers log initializer retrieves all domain
    objects and creates history for those that don't have it (executes a query
    for each one).
    
    It should (if possible) retrieve only some part of domain objects, create
    history for those entries and then retrieve another part.

--- src/main/java/org/openlmis/template/AuditLogInitializer.java
+++ src/main/java/org/openlmis/template/AuditLogInitializer.java
@@ -5,26 +5,33 @@
  * This program is free software: you can redistribute it and/or modify it under the terms
  * of the GNU Affero General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- *  
+ *  
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  * See the GNU Affero General Public License for more details. You should have received a copy of
  * the GNU Affero General Public License along with this program. If not, see
- * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-package org.openlmis.template;
+package org.openlmis;
+
+import static org.openlmis.utils.Pagination.DEFAULT_PAGE_NUMBER;
+import static org.openlmis.utils.Pagination.DEFAULT_PAGE_SIZE;
 
 import org.javers.core.Javers;
 import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.repository.jql.QueryBuilder;
 import org.javers.spring.annotation.JaversSpringDataAuditable;
-import org.openlmis.template.domain.BaseEntity;
+import org.openlmis.requisition.domain.BaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -36,6 +43,7 @@ import java.util.Map;
  * if they haven't already been so. This is, in part, a fix for
  * <a href="https://github.com/javers/javers/issues/214">this issue</a>.
  */
+
 @Component
 @Profile("!test")
 public class AuditLogInitializer implements CommandLineRunner {
@@ -57,29 +65,48 @@ public class AuditLogInitializer implements CommandLineRunner {
 
     //For each one...
     for (Object object : repositoryMap.values()) {
-
-      //... retrieve all of its domain objects and...
-      Iterable<?> domainObjects = ((CrudRepository)(object)).findAll();
-
-      //... for each one...
-      for (Object o : domainObjects) {
-
-        //...check whether there exists a snapshot for it in the audit log.
-        // Note that we don't care about checking for logged changes, per se,
-        // and thus use findSnapshots() rather than findChanges()
-        BaseEntity baseEntity = (BaseEntity)o;
-
-        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(baseEntity.getId(), o.getClass());
-        List<CdoSnapshot> snapshots = javers.findSnapshots(jqlQuery.build());
-
-        //If there are no snapshots of the domain object, then take one
-        if (snapshots.size() == 0) {
-          javers.commit("System: AuditLogInitializer" , baseEntity);
-        }
-
+      if (object instanceof PagingAndSortingRepository) {
+        createSnapshots((PagingAndSortingRepository<?, ?>) object);
+      } else if (object instanceof CrudRepository) {
+        createSnapshots((CrudRepository<?, ?>) object);
       }
     }
+  }
 
+  private void createSnapshots(PagingAndSortingRepository<?, ?> repository) {
+    Pageable pageable = new PageRequest(DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE);
+
+    while (true) {
+      Page<?> page = repository.findAll(pageable);
+
+      if (!page.hasContent()) {
+        break;
+      }
+
+      page.forEach(this::createSnapshot);
+
+      pageable = pageable.next();
+    }
+  }
+
+  private void createSnapshots(CrudRepository<?, ?> repository) {
+    //... retrieve all of its domain objects and...
+    repository.findAll().forEach(this::createSnapshot);
+  }
+
+  private void createSnapshot(Object object) {
+    //...check whether there exists a snapshot for it in the audit log.
+    // Note that we don't care about checking for logged changes, per se,
+    // and thus use findSnapshots() rather than findChanges()
+    BaseEntity baseEntity = (BaseEntity) object;
+
+    QueryBuilder jqlQuery = QueryBuilder.byInstanceId(baseEntity.getId(), object.getClass());
+    List<CdoSnapshot> snapshots = javers.findSnapshots(jqlQuery.build());
+
+    //If there are no snapshots of the domain object, then take one
+    if (snapshots.size() == 0) {
+      javers.commit("System: AuditLogInitializer", baseEntity);
+    }
   }
 
 }
