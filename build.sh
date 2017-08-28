4820b737d3d5355c542cb12ec2de3a17975b98c5
Josh Zamor <josh.zamor@villagereach.org>
2017-08-28 15:07:13 -0700

    fix error where this was overwriting requisition

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Sync with Transifex
 /transifex/sync_transifex.sh \
-  --resource openlmis-requisition.messages \
+  --resource openlmis-template-service.messages \
   --pattern 'src/main/resources/messages_<lang>.properties' \
   --source-file src/main/resources/messages_en.properties
 
