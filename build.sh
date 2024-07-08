8f9b3cccb9e47a07130f2d2652a6fc85b1b80ae4
Szymon Radziszewski <sradziszewski@soldevelo.com>
2024-07-08 07:56:36 +0200

    OIS-14: Added configuration for Transifex CLI usage
    Changed --resource value
    Updated the version of docker-dev used to v9

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Sync with Transifex
 /transifex/sync_transifex.sh \
-  --resource openlmis-template-service.messages \
+  --resource openlmis-template-service \
   --pattern 'src/main/resources/messages_<lang>.properties' \
   --source-file src/main/resources/messages_en.properties
 
